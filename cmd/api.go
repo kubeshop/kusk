15485900118b9bcebbabb5d9153b0ec19ea4fa6c
Kyle Hodgetts <kyle@kubeshop.io>
2021-11-18 19:06:35 +0200

    Update license to MIT

--- cmd/api.go
+++ cmd/api.go
@@ -1,4 +1,6 @@
 /*
+The MIT License (MIT)
+
 Copyright © 2021 Kubeshop
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,6 +20,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
+.
 */
 package cmd
 
