1eff367c0840c5104b6c011016725e0b7da53e1f
Kyle Hodgetts <kyle@kubeshop.io>
2021-11-22 11:18:48 +0200

    Add usage and example docs (#6)
    * Add usage and example docs

--- cmd/generate.go
+++ cmd/generate.go
@@ -30,7 +30,6 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/getkin/kin-openapi/jsoninfo"
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/ghodss/yaml"
 	"github.com/kubeshop/kusk-gateway/options"
@@ -56,8 +55,40 @@ var (
 // generateCmd represents the generate command
 var generateCmd = &cobra.Command{
 	Use:   "generate",
-	Short: "Generate kusk gateway api resources from an OpenAPI spec",
-	Long:  ``,
+	Short: "Generate a Kusk Gateway API resource from your OpenAPI spec file",
+	Long: `
+	Generate accepts your OpenAPI spec file as input either as a local file or a URL pointing to your file
+	and generates a Kusk Gateway compatible API resource that you can apply directly into your cluster.
+
+	Configuration of the API resource is done via the x-kusk extension.
+
+	If the OpenAPI spec doesn't have a top-level x-kusk annotation set, it will add them for you and set
+	the upstream service, namespace and port to the flag values passed in respectively and set the rest of the settings to defaults.
+	This is enough to get you started
+
+	If the x-kusk extension is already present, it will override the the upstream service, namespace and port to the flag values passed in respectively
+	and leave the rest of the settings as they are
+
+	Sample usage
+	
+	No namespace specified
+	kgw api generate -i spec.yaml --name httpbin-api --upstream.service httpbin --upstream.port 8080
+
+	In the above example, as --namespace isn't defined, it will assume the default namespace.
+
+	Namespace specified
+	kgw api generate -i spec.yaml --name httpbin-api --upstream.service httpbin --upstream.namespace my-namespace --upstream.port 8080
+
+	OpenAPI spec at URL
+	kgw api generate \
+			-i https://raw.githubusercontent.com/$ORG_OR_USER/$REPO/myspec.yaml \
+			 --name httpbin-api \
+			 --upstream.service httpbin \
+			 --upstream.namespace my-namespace \
+			 --upstream.port 8080
+	
+	This will fetch the OpenAPI document from the provided URL and generate a Kusk Gateway API resource
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		parsedApiSpec, err := spec.NewParser(openapi3.NewLoader()).Parse(apiSpecPath)
 		if err != nil {
@@ -83,11 +114,6 @@ var generateCmd = &cobra.Command{
 		}
 
 		if err := validateExtensionOptions(parsedApiSpec.ExtensionProps.Extensions["x-kusk"]); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-
-		if err := jsoninfo.NewObjectEncoder().EncodeStructFieldsAndExtensions(&options.Options{}); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
@@ -148,7 +174,7 @@ func init() {
 		"name",
 		"",
 		"",
-		"the name to give the API resource",
+		"the name to give the API resource e.g. --name my-api",
 	)
 	generateCmd.MarkFlagRequired("name")
 
@@ -157,7 +183,7 @@ func init() {
 		"namespace",
 		"n",
 		"default",
-		"the namespace of the API resource",
+		"the namespace of the API resource e.g. --namespace my-namespace, -n my-namespace",
 	)
 
 	generateCmd.Flags().StringVarP(
@@ -165,7 +191,7 @@ func init() {
 		"in",
 		"i",
 		"",
-		"file path to api spec file to generate mappings from. e.g. --in apispec.yaml",
+		"file path or URL to OpenAPI spec file to generate mappings from. e.g. --in apispec.yaml",
 	)
 	generateCmd.MarkFlagRequired("in")
 
