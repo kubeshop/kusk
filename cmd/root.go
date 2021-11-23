5f98ccc46085fd1503ed680ec2e1bfee10faaab0
Kyle Hodgetts <kyle@kubeshop.io>
2021-11-23 13:39:58 +0200

    change config reference to kgw and show help output when api command is run directly (#10)
    * rename all references to kusk-gateway-cli to kgw
    
    * show help output when kgw api is called as it cannot be called directly at the moment

--- cmd/root.go
+++ cmd/root.go
@@ -54,7 +54,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kusk-api-cli.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.kgw.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -71,10 +71,10 @@ func initConfig() {
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
 
-		// Search config in home directory with name ".kusk-api-cli" (without extension).
+		// Search config in home directory with name ".kgw" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".kusk-api-cli")
+		viper.SetConfigName(".kgw")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
