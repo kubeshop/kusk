0903c8a437fce25f8f515fb2175949b2d16df664
Kyle Hodgetts <kyle@kubeshop.io>
2022-06-27 10:12:15 +0100

    always set analytics.enabled value

--- cmd/install.go
+++ cmd/install.go
@@ -217,6 +217,11 @@ func listReleases(helmPath, releaseName, releaseNamespace string) (map[string]st
 }
 
 func installKuskGateway(helmPath, releaseName, releaseNamespace string) error {
+	analyticsEnabled := "true"
+	if enabled, ok := os.LookupEnv("ANALYTICS_ENABLED"); ok {
+		analyticsEnabled = enabled
+	}
+
 	command := []string{
 		"upgrade",
 		"--install",
@@ -225,16 +230,9 @@ func installKuskGateway(helmPath, releaseName, releaseNamespace string) error {
 		"--namespace",
 		releaseNamespace,
 		"--set", fmt.Sprintf("fullnameOverride=%s", releaseName),
+		"--set", fmt.Sprintf("analytics.enabled=%s", analyticsEnabled),
 		releaseName,
 		"kubeshop/kusk-gateway",
-	}
-
-	if enabled, ok := os.LookupEnv("ANALYTICS_ENABLED"); ok && enabled == "false" {
-		command = append(command, "")
-		copy(command[7:], command[6:])
-		command[6] = "--set"
-		copy(command[8:], command[7:])
-		command[7] = fmt.Sprintf("analytics.enabled=%s", enabled)
 	}
 
 	out, err := process.Execute(helmPath, command...)
